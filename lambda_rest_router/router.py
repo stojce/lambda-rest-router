f385a5e4f411618a8f82c83ebedfb5d37cf766ac
stojce <stojce@me.com>
2023-08-02 02:49:53 +0200

    build: linter fixes

--- lambda_rest_router/router.py
+++ lambda_rest_router/router.py
@@ -1,10 +1,14 @@
+""" AWS Lambda Router module """
+
 import re
 from functools import lru_cache
 import importlib
+import base64
+import json
 
 AVAILABLE_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-
+# pylint: disable=too-few-public-methods
 class LambdaRouter:
     """
     AWS Lambda router.
@@ -43,6 +47,45 @@ class LambdaRouter:
         route_parts = path.split("/")
         return f"{route_parts[0]}_{str(len(route_parts) - 1)}"
 
+    def __extract_params(self, event):
+        path = event["pathParameters"]["proxy"]
+        method = event["requestContext"]["http"]["method"]
+
+        query_string = event.get("queryStringParameters", {})
+        body_str = event.get("body", "{}")
+        if body_str and event.get("isBase64Encoded"):
+            body_str = base64.b64decode(body_str)
+        body = json.loads(body_str)
+        request = {**query_string, **body}
+
+        return path, method, request
+
+    @lru_cache
+    def __compiled_routes(self) -> dict:
+        """
+        Returns a dictionary of route hashes and their associated routes.
+        The function iterates over the values of the `routes` dictionary and splits
+        each route by the forward slash ("/").
+        It then creates a unique hash for each route based on the first part of the
+        route and the number of parts in total.
+        If the hash does not exist in the `route_hashes` dictionary, it adds an empty
+        list for that hash.
+        It then appends the route to the list associated with the hash.
+        Finally, it returns the `route_hashes` dictionary.
+
+        :return: A dictionary containing route hashes and their associated routes.
+        :rtype: dict
+        """
+        route_hashes = {}
+
+        for route in self.routes.keys():
+            route_hash = self.__get_route_hash_from_path(route)
+            if route_hash not in route_hashes:
+                route_hashes[route_hash] = []
+            route_hashes[route_hash].append(route)
+
+        return route_hashes
+
     def call_route(self, event, **kwargs):
         """
         Calls the appropriate route handler based on the provided path, method, and request.
@@ -58,17 +101,8 @@ class LambdaRouter:
             The result of calling the route handler method.
         """
 
-        path = event["pathParameters"]["proxy"]
-        method = event["requestContext"]["http"]["method"]
+        path, method, request = self.__extract_params(event)
 
-        query_string = event.get("queryStringParameters", {})
-        body_str = event.get("body", "{}")
-        if body_str and event.get("isBase64Encoded"):
-            body_str = base64.b64decode(body_str)
-        body = json.loads(body_str)
-        request = {**query_string, **body}
-
-        
         call, params = self.__get_matching_route(path, method)
         params.update(request)
 
@@ -92,30 +126,4 @@ class LambdaRouter:
         instance = class_(**kwargs)
         method_ = getattr(instance, class_parts[1])
 
-        # TODO: validate params
         return method_(**params)
-
-    @lru_cache
-    def __compiled_routes(self) -> dict:
-        """
-        Returns a dictionary of route hashes and their associated routes.
-        The function iterates over the values of the `routes` dictionary and splits
-        each route by the forward slash ("/").
-        It then creates a unique hash for each route based on the first part of the
-        route and the number of parts in total.
-        If the hash does not exist in the `route_hashes` dictionary, it adds an empty list for that hash.
-        It then appends the route to the list associated with the hash.
-        Finally, it returns the `route_hashes` dictionary.
-
-        :return: A dictionary containing route hashes and their associated routes.
-        :rtype: dict
-        """
-        route_hashes = {}
-
-        for route in self.routes.keys():
-            route_hash = self.__get_route_hash_from_path(route)
-            if route_hash not in route_hashes:
-                route_hashes[route_hash] = []
-            route_hashes[route_hash].append(route)
-
-        return route_hashes
